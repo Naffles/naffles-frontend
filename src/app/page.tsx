809f1d0a6282623a959e59ddd21cbee988c1709b
a.gestores <a.gestores@prostrive.io>
2024-03-27 18:54:15 +0800

    feat: added page header and tailwind configurations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Button from "@components/shared/Button/button";
+import PageHeader from "@components/shared/PageHeaders/PageHeader";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button
-        label="Click me"
+      <PageHeader
+        // onLogout={() => console.log("Logged out")}
+        // onLogin={() => console.log("Logged in")}
       />
     </main>
   );
