eb8ec0c02bac078fc0d6de24b6d6becdf9a2baca
Arjay Gestores <a.gestores@prostrive.io>
2024-04-16 11:39:45 +0000

    Development

--- src/components/context/BasicUser/BasicUser.tsx
+++ src/components/context/BasicUser/BasicUser.tsx
@@ -5,18 +5,29 @@ import axios from "@components/utils/axios";
 
 // Define the type for the user
 type BasicUser = Record<string, any>;
+type PointsObject = {
+  points: number;
+  date: number;
+};
 // Define the type for the user context.
 type BasicUserContextType = {
   user: BasicUser | null;
   jwt: string | null;
+  points: number;
+  addPoints: (points: number) => void;
   login: (data: LoginParams) => Record<string, any> | void;
   logout: () => void;
 };
+
+const unixToString = (unixTimestamp: number) =>
+  new Date(unixTimestamp).toISOString().split("T")[0];
 
 // Create a context for user data.
 const BasicUserContext = createContext<BasicUserContextType>({
   user: null,
   jwt: null,
+  points: 0,
+  addPoints: (points) => {},
   login: (data) => {},
   logout: () => {},
 });
@@ -35,10 +46,38 @@ export const BasicUserProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const [jwt, setJWT, removeJWT] = useLocalStorage("naffles-auth", "");
+  const [jwt, setJWT, removeJWT] = useLocalStorage("naffles-auth", "", {
+    initializeWithValue: false,
+  });
   const [user, setUser, removeUser] = useLocalStorage<BasicUser | null>(
     "naffles-user",
-    null
+    null,
+    { initializeWithValue: false }
+  );
+  const [pointsObject, setPoints, removePoints] =
+    useLocalStorage<PointsObject | null>("naffles-points", null, {
+      initializeWithValue: false,
+    });
+
+  const addPoints = useCallback(
+    (addedPoints: number) => {
+      setPoints((points) => {
+        if (points?.date) {
+          const currentDateNumber = Date.now();
+          const currentDate = unixToString(currentDateNumber);
+          const previousDate = unixToString(points.date);
+          if (previousDate === currentDate) {
+            const currentPoints = points?.points || 0;
+            return {
+              points: currentPoints + addedPoints,
+              date: currentDateNumber,
+            };
+          }
+        }
+        return { points: addedPoints, date: Date.now() };
+      });
+    },
+    [setPoints]
   );
 
   const login = useCallback(
@@ -51,18 +90,22 @@ export const BasicUserProvider = ({
       });
       setUser(data?.user);
       setJWT(data?.token);
+      setPoints({ points: data?.temporaryPoints || 0, date: Date.now() });
       return data;
     },
-    [setJWT, setUser]
+    [setJWT, setUser, setPoints]
   );
+
   const logout = useCallback(() => {
     removeJWT();
     removeUser();
-  }, [removeJWT, removeUser]);
+    removePoints();
+  }, [removeJWT, removeUser, removePoints]);
 
   const contextValue = useMemo(() => {
-    return { jwt, user, login, logout };
-  }, [jwt, user, login, logout]);
+    const points = pointsObject?.points || 0;
+    return { jwt, user, points, addPoints, login, logout };
+  }, [jwt, user, login, logout, pointsObject, addPoints]);
 
   return (
     <BasicUserContext.Provider value={contextValue}>
