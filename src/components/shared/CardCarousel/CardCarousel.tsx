0979e34fd53c0e5886f35831ae19bdff875d32e5
leonellcruz1107 <l.cruz@prostrive.io>
2024-05-06 13:57:15 +0800

    add nafflings in card carousel

--- src/components/shared/CardCarousel/CardCarousel.tsx
+++ src/components/shared/CardCarousel/CardCarousel.tsx
@@ -10,9 +10,22 @@ const CardCarousel = ({ children }: any) => {
         slidesPerView={"auto"}
         navigation={true}
         modules={[Navigation]}
+        style={{ overflow: "visible" }}
       >
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((item) => (
-          <SwiperSlide key={item} style={{ width: "210px" }}>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((item, index) => (
+          <SwiperSlide
+            key={item}
+            style={{
+              width: "210px",
+              position: "relative",
+              overflow: "visible",
+            }}
+          >
+            {index === 3 && (
+              <div className="absolute z-40 w-[100px] top-[-96px] right-[-10px]">
+                <img src="/nafflings/cigar.png" alt="" />
+              </div>
+            )}
             <Card
               title="Boared Ape"
               description="Yacht Club"
