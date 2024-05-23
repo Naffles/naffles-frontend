8f73a1560093a43ef953d2ff16b0728734cdc767
a.gestores <a.gestores@prostrive.io>
2024-05-23 17:34:59 +0800

    fix: text on tnc and pp

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -44,21 +44,21 @@ const PrivacyPolicy = () => {
       </div>
       <div className="w-full max-w-[900px] max-h-[1200px] h-5/6 mt-4 text-nafl-white overflow-auto font-face-roboto text-body-base font-bold">
         <p className="italic">
-          Last updated: 28December 2023
+          Last updated: 28 December 2023
         </p>
         <br />
         <br />
         <span className="text-body-2xl font-bold italic">BACKGROUND</span>
         <br />
         <br />
-        When a user makes an aggregatelifetime total of depositsexceeding
-        USD10,000orrequestsawithdrawal of any amountinside{" "}
+        When a user makes an aggregate lifetime total of deposits exceeding
+        USD 10,000 or requests a withdrawal of any amount inside{" "}
         <Link href="/" className="font-face-roboto">
           https://naffles.com
         </Link>{" "}
-        the NafflesPlatform, then it is compulsory for them to perform a full
+        the Naffles Platform, then it is compulsory for them to perform a full
         KYC process. During this process, the user will have to input some basic
-        details about themselves and then uploadit as instructed on the Site.
+        details about themselves and then upload it as instructed on the Site.
         The following basic information will be asked from the user to provide:
         <br />
         <br />
@@ -67,7 +67,7 @@ const PrivacyPolicy = () => {
             1)
           </p>
           <p className="ml-4">
-            A copyofGovernment Issued PhotoID;
+            A copy of Government Issued PhotoID;
           </p>
         </div>
         <div className="flex flex-row">
@@ -88,16 +88,16 @@ const PrivacyPolicy = () => {
         </div>
         <br />
         Once uploaded, the user will get a notification that their documents are
-        being processed. Our teamwillthen process the documents andwill email
-        the user the outcome. The outcome will be one of the following: Approval, Rejection, or Request Further Information–No change in Status. When the user’s information is still being processed,they can: Use the platformnormally; They cannot deposit more than USD2,000in aggregate total; and They cannot complete any withdrawal.
+        being processed. Our team will then process the documents and will email
+        the user the outcome. The outcome will be one of the following: Approval, Rejection, or Request Further Information–No change in Status. When the user&apos;s information is still being processed,they can: Use the platformnormally; They cannot deposit more than USD2,000in aggregate total; and They cannot complete any withdrawal.
         <br />
-        When the user’s information is still being processed,they can: Use the
-        platformnormally; They cannot deposit more than USD2,000in aggregate
+        When the user&apos;s information is still being processed,they can: Use the
+        platformnormally; They cannot deposit more than USD2,000 in aggregate
         total; and They cannot complete any withdrawal.
         <br />
         <br />
         <span className="text-body-2xl font-bold italic">
-          I.GUIDELINE FOR THE KYC PROCESS
+          I. GUIDELINE FOR THE KYC PROCESS
         </span>
         <br />
         <br />
@@ -130,7 +130,7 @@ const PrivacyPolicy = () => {
                 c)
               </p>
               <p className="ml-4">
-                Full Name on ID matches client’s nameas when they signed up to use the Site;
+                Full Name on ID matches client&apos;s nameas when they signed up to use the Site;
               </p>
             </div>
             <div className="flex flex-row">
@@ -182,7 +182,7 @@ const PrivacyPolicy = () => {
                 c)
               </p>
               <p className="ml-4">
-                Full Name matches client’s nameas when they signed up to use the Siteand is same as in proof of ID; andd.Date of Issue: In the last 3 months.
+                Full Name matches client&apos;s nameas when they signed up to use the Siteand is same as in proof of ID; andd.Date of Issue: In the last 3 months.
               </p>
             </div>
           </p>
@@ -216,8 +216,8 @@ const PrivacyPolicy = () => {
         </div>
         <br />
         <br />
-        <span className="text-body-2xl font-bold">
-          II.NOTES ON THE KYC PROCESS
+        <span className="text-body-2xl font-bold italic">
+          II. NOTES ON THE KYC PROCESS
         </span>
         <br />
         <br />
@@ -251,7 +251,7 @@ const PrivacyPolicy = () => {
           </p>
         </div>
         <br />
-        <span className="text-body-2xl font-bold">III.OTHERAML MEASURES</span>
+        <span className="text-body-2xl font-bold italic">III. OTHER AML MEASURES</span>
         <br />
         <br />
         <div className="flex flex-row">
