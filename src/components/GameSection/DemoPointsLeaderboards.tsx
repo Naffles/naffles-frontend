6061d314d6768c018551bf6fbd52e38e0421ba0e
pengulang <r.gonzales@prostrive.io>
2024-05-30 15:04:10 +0800

    fix your naffles to your nafflings

--- src/components/GameSection/DemoPointsLeaderboards.tsx
+++ src/components/GameSection/DemoPointsLeaderboards.tsx
@@ -187,7 +187,7 @@ const DemoPointsLeaderboards = () => {
         <div className="flex flex-row items-center justify-between w-full bg-[#444444] rounded-[16px] border-[1px] border-[#464646] drop-shadow-nafl-sponge-2xl h-[54px] px-[14px]">
           <div className="flex flex-col gap-[3px]">
             <p className="font-face-bebas text-[14px] text-[#DC2ABF] leading-[100%]">
-              YOUR NAFFLES
+              YOUR NAFFLINGS
             </p>
             <div className="relative">
               <p className="font-face-bebas text-[20px] text-nafl-white leading-[100%]">
